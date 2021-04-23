00ec2114a0756f31fdaa2b38341b7cf960835964
miiila <me@milavotradovec.cz>
2021-04-23 12:01:32 +0200

    add cloudfront for transformation-service

--- infrastructure/aws/aws.tf
+++ infrastructure/aws/aws.tf
@@ -50,6 +50,11 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "aws" {
+  region = "us-east-1"
+  alias = "lambda"
+}
+
 # ----------------
 # NAT Gateway Section
 # ----------------
@@ -447,6 +452,22 @@ resource "aws_cloudfront_distribution" "distribution" {
     }
   }
 
+  origin {
+    domain_name = aws_lb.service-transformation-lb.dns_name
+    origin_id   = "service-transformation"
+
+    custom_origin_config {
+      http_port              = 8080
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = [
+        "TLSv1",
+        "TLSv1.1",
+        "TLSv1.2"
+      ]
+    }
+  }
+
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
@@ -478,6 +499,44 @@ resource "aws_cloudfront_distribution" "distribution" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
+  ordered_cache_behavior {
+    path_pattern = "/transformation-service/*"
+    allowed_methods  = [
+      "GET",
+      "POST",
+      "DELETE",
+      "OPTIONS",
+      "PUT",
+      "PATCH",
+      "HEAD"]
+    cached_methods   = [
+      "GET",
+      "HEAD"]
+    target_origin_id = "service-transformation"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = aws_lambda_function.lambda_edge_request.qualified_arn
+    }
+
+    lambda_function_association {
+      event_type = "viewer-response"
+      lambda_arn = aws_lambda_function.lambda_edge_response.qualified_arn
+    }
+  }
+
   price_class = "PriceClass_All"
 
   restrictions {
@@ -494,3 +553,41 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 }
 
+resource "aws_lambda_function" "lambda_edge_request" {
+  provider      = aws.lambda
+	publish       = true
+  filename      = "transformation_service_reverse_proxy.zip"
+  function_name = "transformation-service-reverse-proxy-request"
+  role          = aws_iam_role.lambda_edge_exec.arn
+  handler       = "index.request_handler"
+  runtime       = "nodejs12.x"
+
+}
+
+resource "aws_lambda_function" "lambda_edge_response" {
+  provider      = aws.lambda
+	publish       = true
+  filename      = "transformation_service_reverse_proxy.zip"
+  function_name = "transformation-service-reverse-proxy-response"
+  role          = aws_iam_role.lambda_edge_exec.arn
+  handler       = "index.response_handler"
+  runtime       = "nodejs12.x"
+
+}
+
+resource "aws_iam_role" "lambda_edge_exec" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
