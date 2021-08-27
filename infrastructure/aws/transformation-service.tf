59af2800bd309961efe70d7f9c2f0b29b58d60bd
Mila Votradovec <me@milavotradovec.cz>
2021-08-27 20:57:58 +0200

    deploy mail service

--- infrastructure/aws/transformation-service.tf
+++ infrastructure/aws/transformation-service.tf
@@ -11,6 +11,15 @@ resource "aws_ecr_repository" "service-transformation" {
   }
 }
 
+resource "aws_ecr_repository" "service-mail" {
+  name                 = "service-mail"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 # -------------
 # IAM roles
 # -------------
@@ -64,6 +73,19 @@ resource "aws_ecs_task_definition" "service-transformation" {
   cpu                   = "256"
 }
 
+resource "aws_ecs_task_definition" "service-mail" {
+  family                = "service-mail"
+  container_definitions = templatefile("service-mail.tmpl", {
+    aws-region     = var.aws_region,
+    aws-repository = aws_ecr_repository.service-mail.repository_url,
+  })
+  network_mode          = "awsvpc"
+  execution_role_arn    = aws_iam_role.ecs-task-execution-role-transformation-service.arn
+  task_role_arn         = aws_iam_role.ecs-task-execution-role-transformation-service.arn
+  memory                = "512"
+  cpu                   = "256"
+}
+
 resource "aws_ecs_service" "service-transformation" {
   name                               = "service-transformation"
   cluster                            = aws_ecs_cluster.service-transformation.id
@@ -86,6 +108,31 @@ resource "aws_ecs_service" "service-transformation" {
     target_group_arn = aws_lb_target_group.service-transformation-tg.arn
     container_name   = "service-transformation"
     container_port   = 8080
+  }
+}
+
+resource "aws_ecs_service" "service-mail" {
+  name                               = "service-mail"
+  cluster                            = aws_ecs_cluster.service-transformation.id
+  task_definition                    = aws_ecs_task_definition.service-mail.arn
+  launch_type                        = "FARGATE"
+  desired_count                      = 1
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  health_check_grace_period_seconds  = 20
+
+  network_configuration {
+    security_groups = [
+      aws_security_group.transformation-service-sg.id]
+    subnets         = [
+      aws_subnet.private.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.service-mail-tg.arn
+    container_name   = "service-mail"
+    container_port   = 8081
   }
 }
 
@@ -122,6 +169,17 @@ resource "aws_lb_listener" "service-transformation-elb-listener" {
   }
 }
 
+resource "aws_lb_listener" "service-mail-elb-listener" {
+  load_balancer_arn = aws_lb.service-transformation-lb.arn
+  port              = "8081"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.service-mail-tg.arn
+  }
+}
+
 resource "aws_lb_target_group" "service-transformation-tg" {
   name        = "service-transformation-tg"
   port        = 8080
@@ -131,6 +189,19 @@ resource "aws_lb_target_group" "service-transformation-tg" {
   health_check {
     enabled = true
     port    = 8080
+    matcher = "200,404"
+  }
+}
+
+resource "aws_lb_target_group" "service-mail-tg" {
+  name        = "service-mail-tg"
+  port        = 8081
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+  target_type = "ip"
+  health_check {
+    enabled = true
+    port    = 8081
     matcher = "200,404"
   }
 }
